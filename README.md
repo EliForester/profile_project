4305134dbe953e7806a3335c6e3298db0bfd3ebc
EliForester <eli.j.forester@gmail.com>
2018-05-21 18:01:13 +0900

    Formatting & making pretty. Plus README

---
+++ README.md
@@ -0,0 +1,31 @@
+# Profile Project
+Project to create/use user profile
+
+## Getting Started
+
+Clone the repository
+
+Run the following:
+
+```
+  python manage.py makemigrations
+  python manage.py migrate
+  python manage.py runserver 0.0.0.0:8000
+```
+
+### Prerequisites
+
+Created on Python 3 w Django 2
+
+requirements.txt
+```
+Django==2.0.3
+Pillow==5.1.0
+pytz==2018.3
+
+```
+
+### Notes
+
+DEBUG is set to True because setting it false will also validate ALLOWED_HOSTS
+No extra credit attempted
--- profile_project/accounts/urls.py
+++ profile_project/accounts/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'sign_in/$', views.sign_in, name='sign_in'),
     url(r'sign_up/$', views.sign_up, name='sign_up'),
     url(r'sign_out/$', views.sign_out, name='sign_out'),
-    url(r'change_password/', views.change_password, name='change_password'),
-    url(r'edit/', views.edit, name='edit'),
-    url(r'profile/', views.profile, name='profile'),
+    url(r'change_password/$', views.change_password, name='change_password'),
+    url(r'edit/$', views.edit, name='edit'),
+    url(r'profile/$', views.profile, name='profile'),
 ]
